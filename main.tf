3ee149a974b6eef5529687fe6c824d8589757239
Peter ONeill <peteroneilljr@gmail.com>
2022-06-02 10:43:03 +0100

    blast radius 32

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  for_each = toset(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "11", "12", "13", "14", "15"])
+  for_each = toset(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "11", "12", "13", "14", "15", "16"])
   # for_each = toset(["one", "two", "three"])
   name     = "instance-${each.key}"
 
