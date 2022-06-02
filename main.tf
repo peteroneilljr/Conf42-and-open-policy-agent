93fdfe76f6afdc8f06d0d8c47bf57f94cee40415
Peter ONeill <peteroneilljr@gmail.com>
2022-06-02 10:49:32 +0100

    double equals

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "ec2_instance" {
 
   for_each = toset(["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "11", "12", "13", "14", "15", "16"])
   # for_each = toset(["one", "two", "three"])
-  name     = "instance-${each.key}"
+  name = "instance-${each.key}"
 
   ami                    = "ami-ebd02392"
   instance_type          = "t3.micro"
